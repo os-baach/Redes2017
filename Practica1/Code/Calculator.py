f53609677675ce1274329a98ef7b9d141b9d73e3
Victor Zamora <victorzamoragutierrez@gmail.com>
2016-08-13 00:00:38 -0500

    Agrega calculadora que suma y resta

---
+++ Practica1/Code/Calculator.py
@@ -0,0 +1,13 @@
+# Clase que representa a una calculadora que suma y resta
+class Calculator:
+    
+    # Suma
+    def suma(self, n1, n2):
+        return n1+n2
+        
+    # Resta
+    def resta(self, n1, n2):
+        return n1-n2
+        
+        
+        
