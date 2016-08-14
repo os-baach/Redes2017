befac5b288d30416ba3a316d5a664660d072a100
Victor Zamora <victorzamoragutierrez@gmail.com>
2016-08-14 11:41:53 -0500

    Ya sirven las calculadoras

--- Practica1/Code/Calculator.py
+++ Practica1/Code/Calculator.py
@@ -3,11 +3,11 @@ class Calculator:
     
     # Suma
     def suma(self, n1, n2):
-        return float(n1+n2)
+        return n1+n2
         
     # Resta
     def resta(self, n1, n2):
-        return float(n1-n2)
+        return n1-n2
         
         
         
