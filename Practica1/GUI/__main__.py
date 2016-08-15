7222fd1cd7bffa00cf8163bd5eb3fa54f585a26b
ricardorodab <ricardorodab19@gmail.com>
2016-08-15 10:01:04 -0500

    Cambios gui

---
+++ Practica1/GUI/__main__.py
@@ -0,0 +1,12 @@
+import sys
+from PyQt4 import QtGui
+
+app = QtGui.QApplication(sys.argv)
+
+window = QtGui.QWidget()
+window.setGeometry(0, 0, 500, 300)
+window.setWindowTitle("PyQT Tuts!")
+
+window.show()
+sys.exit(app.exec_())
+
